382278f7cbbd2a1da30458206e27158f07a923ef
Yasuhiro Osajima <kimukimu33@gmail.com>
2019-06-02 09:31:21 +0000

    update unit test for workflow YAML parser

--- tests/internal/workflow/test_parser.py
+++ tests/internal/workflow/test_parser.py
@@ -1,15 +1,38 @@
 #!/usr/bin/env python3
-"""
-Unit test for workflow parser.
-"""
+""" Unit test for workflow parser """
 
 import unittest
+
+import yaml
 
 from internal.workflow import tree
 
 
 class TestWorkFlowParser(unittest.TestCase):
+    """ Unit test for workflow parser """
+
+    def test_generate_workflow_dict(self):
+        """ Test case for YAML file convert to dict """
+
+        workflow_yml = """
+---
+- job_template: sample_job1
+  success:
+    - job_template: sample_job2
+  always:
+    - job_template: sample_job3"""
+
+        result = yaml.load(workflow_yml, Loader=yaml.SafeLoader)
+
+        correct = [{'job_template': 'sample_job1',
+                    'success': [{'job_template': 'sample_job2'}],
+                    'always': [{'job_template': 'sample_job3'}]}]
+
+        self.assertEqual(result, correct)
+
     def test_workflow_parse(self):
+        """ Test case dict convert to workflow Node obj """
+
         workflow = [{'job_template': 'sample_job1',
                      'success': [{'job_template': 'sample_job2'}],
                      'always': [{'job_template': 'sample_job3'}]}]
